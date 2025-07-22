05fbd1fa74bd7eb7597e0b208e4cb32c2fd2494a
Jack Wilkinson <wilkinsonjthomas@gmail.com>
2025-07-22 13:49:30 -0600

    Making it homey

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    // Add domains or settings here if you use remote images
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
